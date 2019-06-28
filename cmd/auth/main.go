2401bc8c4296ceb9a964f398b268af0a9e2f4d9d
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-06-28 17:31:09 +0300

    Adding simple cli interface for auth package.

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/url"
+	"os"
 
 	"github.com/evgeny-myasishchev/ledger.transactions-fetcher/config"
 	"github.com/evgeny-myasishchev/ledger.transactions-fetcher/pkg/lib-core-golang/diag"
@@ -10,20 +12,28 @@ import (
 
 var logger = diag.CreateLogger()
 
-func main() {
-	svcCfg := config.Load()
+var cliArgs struct {
+	cmd string
+}
 
-	diag.SetupLoggingSystem(func(setup diag.LoggingSystemSetup) {
-		setup.SetLogLevel(svcCfg.StringParam(config.LogLevel).Value())
-	})
+func init() {
+	const (
+		defaultGopher = "pocket"
+		usage         = "the variety of gopher"
+	)
+	flag.StringVar(&cliArgs.cmd, "cmd", "", "Command to run. Available commands: auth-url")
 
+	flag.Parse()
+}
+
+func printAuthUrl(googleClientID string) {
 	authURL, err := url.Parse("https://accounts.google.com/o/oauth2/v2/auth")
 	if err != nil {
 		panic(err)
 	}
 	query := authURL.Query()
 	query.Add("response_type", "code")
-	query.Add("client_id", svcCfg.StringParam(config.GoogleClientID).Value())
+	query.Add("client_id", googleClientID)
 	query.Add("redirect_uri", "urn:ietf:wg:oauth:2.0:oob")
 	query.Add("scope", "email")
 	query.Add("access_type", "offline")
@@ -33,4 +43,27 @@ func main() {
 	fmt.Println("Then follow the instruction and use rake get-access-token[code] with the code displayed")
 	fmt.Println("The url:")
 	fmt.Println(authURL)
+}
+
+func main() {
+	fmt.Println(cliArgs)
+
+	if cliArgs.cmd == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	svcCfg := config.Load()
+
+	diag.SetupLoggingSystem(func(setup diag.LoggingSystemSetup) {
+		setup.SetLogLevel(svcCfg.StringParam(config.LogLevel).Value())
+	})
+
+	switch cliArgs.cmd {
+	case "auth-url":
+		printAuthUrl(svcCfg.StringParam(config.GoogleClientID).Value())
+	default:
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
