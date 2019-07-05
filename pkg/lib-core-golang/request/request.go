a0e96affd99fa5e923636138b07981cd0e0045c4
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2019-07-05 09:00:29 +0300

    Request logging.

--- pkg/lib-core-golang/request/request.go
+++ pkg/lib-core-golang/request/request.go
@@ -2,8 +2,10 @@ package request
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/evgeny-myasishchev/ledger.transactions-fetcher/pkg/lib-core-golang/diag"
 )
@@ -21,6 +23,19 @@ func withLogger(logger diag.Logger) SendOpt {
 	return func(cfg *sendCfg) {
 		cfg.logger = logger
 	}
+}
+
+func flattenAndObfuscate(values map[string][]string, obfuscateKeys ...string) map[string]string {
+	flattened := make(map[string]string, len(values))
+	for key, val := range values {
+		flattened[key] = strings.Join(val, ", ")
+	}
+	for _, obfuscateKey := range obfuscateKeys {
+		if val, ok := flattened[obfuscateKey]; ok {
+			flattened[obfuscateKey] = fmt.Sprint("*obfuscated, length=", len(val), "*")
+		}
+	}
+	return flattened
 }
 
 // ReqFactory is a function that creates an instance of a request
@@ -57,6 +72,14 @@ func newResFactory(res *http.Response, err error) ResFactory {
 
 // Do will send the request. Will fail if response status is other than 2xx
 func Do(ctx context.Context, factory ReqFactory, opts ...SendOpt) ResFactory {
+	// TODO: Include requestID header from context
+
+	cfg := &sendCfg{
+		logger: defaultLogger,
+	}
+	for _, opt := range opts {
+		opt(cfg)
+	}
 	httpClient := &http.Client{
 		Transport: http.DefaultTransport,
 	}
@@ -64,5 +87,27 @@ func Do(ctx context.Context, factory ReqFactory, opts ...SendOpt) ResFactory {
 	if err != nil {
 		return newResFactory(nil, err)
 	}
-	return newResFactory(httpClient.Do(req))
+	cfg.logger.
+		WithData(diag.MsgData{
+			"protocol": req.URL.Scheme,
+			"url":      req.URL.String(),
+			"qs":       flattenAndObfuscate(req.URL.Query()),
+
+			// TODO: Obfuscate some headers
+			"headers": flattenAndObfuscate(req.Header),
+
+			"method": req.Method,
+		}).
+		Info(ctx, "SEND REQUEST START")
+	res, err := httpClient.Do(req)
+	cfg.logger.
+		WithData(diag.MsgData{
+			"url":        req.URL.String(),
+			"httpStatus": res.StatusCode,
+			"method":     req.Method,
+
+			// TODO: Res headers
+		}).
+		Info(ctx, "SEND REQUEST COMPLETE")
+	return newResFactory(res, err)
 }
--- pkg/lib-core-golang/request/request_test.go
+++ pkg/lib-core-golang/request/request_test.go
@@ -2,10 +2,14 @@ package request
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"testing"
 	"time"
+
+	"github.com/evgeny-myasishchev/ledger.transactions-fetcher/pkg/lib-core-golang/diag"
 
 	"gopkg.in/h2non/gock.v1"
 
@@ -13,6 +17,38 @@ import (
 
 	"github.com/bxcodec/faker/v3"
 )
+
+type mockLogger struct {
+	messages       []string
+	msgDataEntries []diag.MsgData
+}
+
+func newMockLogger() *mockLogger {
+	return &mockLogger{
+		messages:       []string{},
+		msgDataEntries: []diag.MsgData{},
+	}
+}
+
+func (l *mockLogger) Error(ctx context.Context, msg string, args ...interface{}) {
+	l.messages = append(l.messages, fmt.Sprintf(msg, args...))
+}
+func (l *mockLogger) Warn(ctx context.Context, msg string, args ...interface{}) {
+	l.messages = append(l.messages, fmt.Sprintf(msg, args...))
+}
+func (l *mockLogger) Info(ctx context.Context, msg string, args ...interface{}) {
+	l.messages = append(l.messages, fmt.Sprintf(msg, args...))
+}
+func (l *mockLogger) Debug(ctx context.Context, msg string, args ...interface{}) {
+	l.messages = append(l.messages, fmt.Sprintf(msg, args...))
+}
+func (l *mockLogger) WithError(err error) diag.Logger {
+	return l
+}
+func (l *mockLogger) WithData(data diag.MsgData) diag.Logger {
+	l.msgDataEntries = append(l.msgDataEntries, data)
+	return l
+}
 
 func TestDo(t *testing.T) {
 	rand.Seed(time.Now().Unix())
@@ -80,6 +116,57 @@ func TestDo(t *testing.T) {
 					return
 				}
 				assert.Equal(t, status, httpErr.StatusCode)
+			}
+		},
+
+		func() (string, tcFn) {
+			return "should log req start/end", func(t *testing.T) {
+				reqURLString := faker.URL() + fmt.Sprintf("?qs1=%v&qs2=%v", faker.Word(), faker.Word())
+				expectedBody := faker.Sentence()
+
+				status := 199 + rand.Intn(100)
+				log := newMockLogger()
+
+				gock.New(reqURLString).
+					Get("/").
+					Reply(status).
+					BodyString(expectedBody)
+
+				reqFactory := Get(reqURLString)
+				resp := Do(context.TODO(), reqFactory, withLogger(log))
+
+				req, err := reqFactory()
+				if !assert.NoError(t, err) {
+					return
+				}
+
+				_, err = resp()
+				if !assert.NoError(t, err) {
+					return
+				}
+
+				reqURL, err := url.Parse(reqURLString)
+				if !assert.NoError(t, err) {
+					return
+				}
+
+				assert.Len(t, log.messages, 2)
+				assert.Len(t, log.msgDataEntries, 2)
+				assert.Equal(t, "SEND REQUEST START", log.messages[0])
+				assert.Equal(t, diag.MsgData{
+					"protocol": reqURL.Scheme,
+					"url":      reqURLString,
+					"qs":       flattenAndObfuscate(reqURL.Query()),
+					"headers":  flattenAndObfuscate(req.Header),
+					"method":   "GET",
+				}, log.msgDataEntries[0])
+
+				assert.Equal(t, "SEND REQUEST COMPLETE", log.messages[1])
+				assert.Equal(t, diag.MsgData{
+					"url":        reqURLString,
+					"method":     "GET",
+					"httpStatus": status,
+				}, log.msgDataEntries[1])
 			}
 		},
 
